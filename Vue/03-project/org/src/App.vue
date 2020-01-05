c857dd11ac4b41a63e9b4856cc89e49cf8c7cf72
shuai-ang <1914551759@qq.com>
2020-01-05 17:58:05 +0800

    'vue-router'

---
+++ Vue/03-project/org/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+       <Header />
+       <List />
+       <Footer />
+    </div>
+</template>
+
+<script>
+    import Header from './pages/header.vue'
+    import List from './pages/list.vue'
+    import Footer from './pages/footer.vue'
+
+    export default{
+      name:'App',
+      components:{
+          Header,
+          List,
+          Footer
+      },
+      data(){
+          return {
+              todos:[
+                  {
+                      task:'吃饭',
+                      done:false
+                  },
+                  {
+                      task:'游泳',
+                      done:false
+                  },
+                  {
+                      task:'散步',
+                      done:false
+                  }
+              ]
+          }
+      },
+      methods:{
+
+      }
+    }
+</script>
+
+<style>
+    #app{
+        width:600px;
+        margin:50px auto;
+    }
+</style>
---
+++ Vue/03-project/org/src/pages/header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="header">
+       <h2>todoList</h2>
+       <input type="text">
+    </div>
+</template>
+
+<script>
+    import Header from './pages/header.vue'
+    import List from './pages/list.vue'
+    import Footer from './pages/footer.vue'
+
+    export default{
+      name:'App',
+      components:{
+          Header,
+          List,
+          Footer
+      },
+      data(){
+          return {
+              todos:[
+                  {
+                      task:'吃饭',
+                      done:false
+                  },
+                  {
+                      task:'游泳',
+                      done:false
+                  },
+                  {
+                      task:'散步',
+                      done:false
+                  }
+              ]
+          }
+      },
+      methods:{
+
+      }
+    }
+</script>
+
+<style>
+    #app{
+        width:600px;
+        margin:50px auto;
+    }
+</style>
